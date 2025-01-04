b6c610750e707365164f3734ea3b1618d84b4950
Lucas Duarte <lukvig@hotmail.com>
2025-01-03 21:26:13 -0300

    feat(main): adiciona grafico em barra das categorias

--- .venv/main.py
+++ .venv/main.py
@@ -25,6 +25,7 @@ co9 = "#e9edf5"
 colors = ['#5588bb', '#66bbbb','#99bb55', '#ee9944', '#444466', '#bb5555']
 
 # cria janela
+
 janela = Tk()
 janela.title()
 janela.geometry('900x650')
@@ -35,6 +36,7 @@ style = ttk.Style(janela)
 style.theme_use("clam")
 
 # criando frames para divisao da tela
+
 frameCima = Frame(janela, width=1043, height=50, bg=co1, relief="flat")
 frameCima.grid(row=0, column=0)
 
@@ -46,6 +48,7 @@ frameBaixo.grid(row=2, column=0, pady=0, padx=10, sticky=NSEW)
 
 # frame cima
 # acessa imagem
+
 app_img = Image.open('dinosaur.png')
 app_img = app_img.resize((45,45))
 app_img = ImageTk.PhotoImage(app_img)
@@ -73,5 +76,49 @@ def porcentagem():
     l_porcentagem = Label(frameMeio, text="{:,.2f}%".format(valor), anchor=NW, font=('Verdana 12'), bg=co1, fg=co4)
     l_porcentagem.place(x=200, y=35)
 
+# funcao para grafico bars
+
+def grafico_bar():
+    lista_categorias = ['Renda', 'Despesas', 'Saldo']
+    lista_valores = [3000, 2000, 6236]
+
+    # faca a figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(4, 3.45), dpi=60)
+    ax = figura.add_subplot(111)
+    # ax.autoscale(enable=True, axis='both', tight=None)
+
+    ax.bar(lista_categorias, lista_valores, color=colors, width=0.9)
+    # create a list to collect the plt.patches data
+
+    c = 0
+    #  set individual bar lables using above list
+    for i in ax.patches:
+        # get_x pulls left or right; get_height pushes up or down
+        ax.text(i.get_x()-.001, i.get_height()+.5,
+                str("{:,.0f}".format(lista_valores[c])), fontsize=17, fontstyle='italic',  verticalalignment='bottom',color='dimgrey')
+        c += 1
+
+    ax.set_xticklabels(lista_categorias,fontsize=16)
+
+    ax.patch.set_facecolor('#ffffff')
+    ax.spines['bottom'].set_color('#CCCCCC')
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(0)
+    ax.spines['top'].set_linewidth(0)
+    ax.spines['left'].set_color('#CCCCCC')
+    ax.spines['left'].set_linewidth(1)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(False, color='#EEEEEE')
+    ax.xaxis.grid(False)
+
+    canva = FigureCanvasTkAgg(figura, frameMeio)
+    canva.get_tk_widget().place(x=10, y=70)
+
 porcentagem()
+grafico_bar()
 janela.mainloop()
